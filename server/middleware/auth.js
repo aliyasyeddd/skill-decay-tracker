d079b1dc49d0073460cbd8185e9ff4bf4b347e0e
aliya mahaboob <122204472+aliyasyeddd@users.noreply.github.com>
2026-09-26 20:57:16 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,8 +3,10 @@ const User = require("../models/User");
 
 const userAuth = async (req, res, next) => {
     try {
-        let token;
 
+        // Check for token in Authorization header or cookies
+        let token;
+        
         const authHeader = req.headers.authorization;
         if (authHeader && authHeader.startsWith("Bearer ")) {
             token = authHeader.split(" ")[1];
@@ -12,19 +14,25 @@ const userAuth = async (req, res, next) => {
             token = req.cookies.token;
         }
 
+        // If no token is found, return an error
         if (!token) {
             return res.status(401).json({ message: "Please Login!" });
         }
 
+        // Verify the token and extract the user ID
         const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
         const { _id } = decodedObj;
 
+        // Find the user in the database using the extracted user ID
         const user = await User.findById(_id);
+        // If the user is not found, return an error
         if (!user) {
             throw new Error("User not found");
         }
 
+        // Attach the user object to the request for further use in the route handlers
         req.user = user;
+        // Call the next middleware or route handler
         next();
     } catch (err) {
         res.status(401).json({ message: "Invalid token" });
